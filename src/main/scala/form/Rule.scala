30ca00a127d23e32730a69ad890f6ab62cf24442
Dave Gurnell <dave@untyped.com>
2016-07-22 17:04:09 +0100

    Fix bugs

--- src/main/scala/form/Rule.scala
+++ src/main/scala/form/Rule.scala
@@ -1,16 +1,13 @@
 package form
 
 sealed trait Rule[A, B] {
-  def apply(value: A): Result[B] = this match {
-    case ZipRule(rule1, rule2)     => rule1(value).zip(rule2(value))
-    case PureRule(func)            => func(value)
-    case MapRule(rule, func)       => rule(value).map(func)
-    case FlatMapRule(rule, func)   => rule(value).flatMap(ans => func(ans).apply(value))
-    case AndThenRule(rule1, rule2) => rule1(value).flatMap(rule2.apply)
-  }
+  def apply(value: A): Result[B]
 
   def map[C](func: B => C): Rule[A, C] =
     MapRule(this, func)
+
+  def flatMap[C](func: B => Rule[A, C]): Rule[A, C] =
+    FlatMapRule(this, func)
 
   def andThen[C](that: Rule[B, C]): Rule[A, C] =
     AndThenRule(this, that)
@@ -19,11 +16,31 @@ sealed trait Rule[A, B] {
     ZipRule(this, that)
 }
 
-final case class PureRule   [X, Y]   (func: X => Result[Y])                    extends Rule[X, Y]
-final case class MapRule    [X, Y, Z](rule: Rule[X, Y], func: Y => Z)          extends Rule[X, Z]
-final case class FlatMapRule[X, Y, Z](rule: Rule[X, Y], func: Y => Rule[X, Z]) extends Rule[X, Z]
-final case class AndThenRule[X, Y, Z](rule1: Rule[X, Y], rule2: Rule[Y, Z])    extends Rule[X, Z]
-final case class ZipRule    [X, Y, Z](rule1: Rule[X, Y], rule2: Rule[X, Z])    extends Rule[X, (Y, Z)]
+final case class PureRule[A, B](func: A => Result[B]) extends Rule[A, B] {
+  def apply(value: A): Result[B] =
+    func(value)
+}
+
+final case class MapRule[A, B, C](rule: Rule[A, B], func: B => C)  extends Rule[A, C] {
+  def apply(value: A): Result[C] =
+    rule(value) map (func)
+}
+
+final case class FlatMapRule[A, B, C](rule: Rule[A, B], func: B => Rule[A, C]) extends Rule[A, C] {
+  def apply(value: A): Result[C] =
+    rule(value) flatMap (ans => func(ans)(value))
+}
+
+final case class AndThenRule[A, B, C](rule1: Rule[A, B], rule2: Rule[B, C]) extends Rule[A, C] {
+  def apply(value: A): Result[C] =
+    rule1(value) flatMap (rule2.apply)
+}
+
+final case class ZipRule[A, B, C](rule1: Rule[A, B], rule2: Rule[A, C]) extends Rule[A, (B, C)] {
+  def apply(value: A): Result[(B, C)] =
+    rule1(value) zip rule2(value)
+}
+
 
 object Rule {
   def pure[A, B](func: A => Result[B]): Rule[A, B] =
